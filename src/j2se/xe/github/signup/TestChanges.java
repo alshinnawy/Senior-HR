134ac03cd9f9c3e39efa6cafb0dada82fcde625d
- <as3ied@hotmail.com>
2016-10-13 08:43:38 +0200

    Adding test changes class

---
+++ src/j2se/xe/github/signup/TestChanges.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j2se.xe.github.signup;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestChanges {
+    public static void main(String[] args) {
+        System.out.println("hiii");
+    }
+}
